43f80efc090a557276f176a9287052b0f9585a32
middledeveloper <nikolay.tikhvin@gmail.com>
2019-10-31 19:31:10 +0300

    Lesson4 v1.3

--- lesson4/train.rb
+++ lesson4/train.rb
@@ -11,64 +11,28 @@ class Train
   end
 
   def accelerate(speed)
-    accelerate!(speed)
-  end
-
-  def break
-    break!
-  end
-
-  def add_wagon(wagon)
-    add_wagon!(wagon)
-  end
-
-  def remove_wagon(wagon)
-    remove_wagon!(wagon)
-  end
-
-  def set_route(route)
-    set_route!(route)
-  end
-
-  def move_forward
-    move_forward!
-  end
-
-  def move_backward
-    move_backward!
-  end
-
-  private
-
-  # Вынесено в private т.к. методы осуществляют прямое присваивание
-  # значений переменным экземпляра класса. Публичные методы оставляют
-  # возможность переопределения методов при необходимости.
-
-  attr_writer :speed, :route, :station
-
-  def accelerate!(speed)
     self.speed += speed if speed > ZERO_SPEED
   end
 
-  def break!
+  def break
     self.speed = ZERO_SPEED
   end
 
-  def add_wagon!(wagon)
+  def add_wagon(wagon)
     wagons.push(wagon) if type == wagon.type && speed.zero?
   end
 
-  def remove_wagon!(wagon)
+  def remove_wagon(wagon)
     wagons.delete(wagon) if speed.zero? && wagons.count > 0
   end
 
-  def set_route!(route)
+  def set_route(route)
     self.route = route
     route.stations.first.add_train(self)
     self.station = route.stations.first
   end
 
-  def move_forward!
+  def move_forward
     return if route.nil?
     return if station == route.stations.last
 
@@ -77,7 +41,7 @@ class Train
     station.add_train(self)
   end
 
-  def move_backward!
+  def move_backward
     return if route.nil?
     return if train.station == train.route.stations.first
 
@@ -85,4 +49,11 @@ class Train
     self.station = route.stations[route.stations.index(station).to_i - 1]
     station.add_train(self)
   end
+
+  private
+
+  # Вынесено в private т.к. осуществляется прямое присваивание
+  # значений переменным экземпляра класса.
+
+  attr_writer :speed, :route, :station
 end
--- lesson4/ui.rb
+++ lesson4/ui.rb
@@ -31,125 +31,21 @@ class UI
     while choice != 0
       case choice
       when 1
-        print 'Введите наименование станции: '
-        station = Station.new(gets.chomp)
-        station_repo.push(station)
-        puts station_repo
-
+        create_station
       when 2
-        puts '1. Пассажирский'
-        puts '2. Грузовой'
-        print 'Укажите тип поезда: '
-        type_value = get_choice(gets.chomp)
-        print 'Необходимо указать номер поезда: '
-        num_value = get_choice(gets.chomp)
-
-        if type_value == 1
-          train = PassengerTrain.new(num_value)
-        elsif type_value == 2
-          train = CargoTrain.new(num_value)
-        end
-
-        train_repo.push(train)
-        puts train_repo
-
+        create_train
       when 3
-        if station_repo.count < 2
-          puts 'Недостаточно станций для создания маршрута'
-        else
-          station_repo.each { |s| puts "#{station_repo.index(s) + 1}. Станция '#{s.name}'" }
-
-          print 'Необходимо указать начальную станцию: '
-          first_station = station_repo[get_choice(gets.chomp) - 1]
-          print 'Необходимо указать конечную станцию: '
-          last_station = station_repo[get_choice(gets.chomp) - 1]
-
-          route = Route.new(first_station, last_station)
-          route_repo.push(route)
-          puts route_repo
-        end
+        create_route
       when 4
-        if train_repo.count.zero?
-          puts 'Не создано ни одного поезда'
-        elsif route_repo.count.zero?
-          puts 'Не создано ни одного маршрута'
-        else
-          train_repo.each { |t| puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}', #{t.type}" }
-          print 'Укажите какому поезду следует задать маршрут: '
-          train_add_route = train_repo[get_choice(gets.chomp) - 1]
-          route_repo.each do |r|
-            puts "Маршрут #{route_repo.index(r) + 1}."
-            puts r.stations
-          end
-
-          print 'Укажите маршрут для добавления: '
-          train_add_route.set_route(route_repo[get_choice(gets.chomp) - 1])
-          puts train_add_route.inspect
-        end
+        set_route
       when 5
-        train_repo.each { |t| puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}' (#{t.type}), вагонов: #{t.wagons.count}" }
-        print 'Укажите к какому поезду следует добавить вагон: '
-        train_add_wagon = train_repo[get_choice(gets.chomp) - 1]
-        wagon_repo.each do |w|
-          next unless w.type == train_add_wagon.type
-
-          wagon_used = wagon_used?(w)
-          unless wagon_used
-            puts "#{wagon_repo.index(w) + 1}. Вагон, тип '#{w.type}'"
-          end
-        end
-        print 'Укажите вагон для добавления к поезду: '
-        wagon_to_add = wagon_repo[get_choice(gets.chomp) - 1]
-        train_add_wagon.add_wagon(wagon_to_add)
-        puts "Количество вагонов поезда #{train_add_wagon.number}: #{train_add_wagon.wagons.count}"
-
+        add_wagon
       when 6
-        train_repo.each do |t|
-          if t.wagons.count > 0
-            puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}' (#{t.type}), вагонов: #{t.wagons.count}"
-          end
-        end
-        print 'Укажите от какого поезда следует отцепить вагон: '
-        train_remove_wagon = train_repo[get_choice(gets.chomp) - 1]
-        puts "Поезд номер #{train_remove_wagon.number} выбран"
-        train_remove_wagon.remove_wagon(train_remove_wagon.wagons.last)
-        puts "Количество вагонов поезда #{train_remove_wagon.number}: #{train_remove_wagon.wagons.count}"
-
+        remove_wagon
       when 7
-        train_repo.each do |t|
-          unless t.route.nil?
-            puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}', #{t.type}"
-          end
-        end
-        print 'Укажите поезд для передвижения: '
-        train_to_move = train_repo[get_choice(gets.chomp) - 1]
-        puts '1. Движение вперед'
-        puts '2. Движение назад'
-        print 'Укажите направление движения поезда: '
-        direction = get_choice(gets.chomp)
-
-        if direction == 1
-          if train_to_move.station != train_to_move.route.stations.last
-            train_to_move.move_forward
-          else
-            puts 'Поезд на конечной станции'
-          end
-        elsif direction == 2
-          if train_to_move.station != train_to_move.route.stations.first
-            train_to_move.move_backward
-          else
-            puts 'Поезд на начальной станции'
-          end
-        end
-
-        puts "Поезд #{train_to_move.number} находится на станции #{train_to_move.station.name}"
-
+        move_train
       when 8
-        station_repo.each do |s|
-          puts "Количество поездов на станции '#{s.name}': #{s.trains.count}"
-          s.trains.each { |t| puts t.number } if s.trains.count > 0
-        end
-
+        station_monitor
       else
         print 'Некорректное значение, повторите ввод!'
         choice = get_choice(gets.chomp)
@@ -160,6 +56,10 @@ class UI
       choice = get_choice(gets.chomp)
     end
   end
+
+  private
+
+  # Вынесено в private т.к. методы не используются за пределами класса UI
 
   def show_main_menu
     puts '1. Создать станцию'
@@ -172,6 +72,137 @@ class UI
     puts '8. Просмотр станций и поездов'
     puts '0. ВЫХОД'
     print 'Введите номер необходимого действия: '
+  end
+
+  def create_station
+    print 'Введите наименование станции: '
+    station = Station.new(gets.chomp)
+    station_repo.push(station)
+    puts 'Перечень станций железной дороги:'
+    puts station_repo.each { |s| puts s.name }
+  end
+
+  def create_train
+    puts '1. Пассажирский'
+    puts '2. Грузовой'
+    print 'Укажите тип поезда: '
+    type_value = get_choice(gets.chomp)
+    print 'Необходимо указать номер поезда: '
+    num_value = gets.chomp
+
+    if type_value == 1
+      train = PassengerTrain.new(num_value)
+    elsif type_value == 2
+      train = CargoTrain.new(num_value)
+    end
+
+    train_repo.push(train)
+    puts 'Перечень поездов железной дороги:'
+    puts train_repo.each { |t| puts t.number }
+  end
+
+  def create_route
+    if station_repo.count < 2
+      puts 'Недостаточно станций для создания маршрута'
+    else
+      station_repo.each { |s| puts "#{station_repo.index(s) + 1}. Станция '#{s.name}'" }
+
+      print 'Необходимо указать начальную станцию: '
+      first_station = station_repo[get_choice(gets.chomp) - 1]
+      print 'Необходимо указать конечную станцию: '
+      last_station = station_repo[get_choice(gets.chomp) - 1]
+
+      route = Route.new(first_station, last_station)
+      route_repo.push(route)
+      puts route_repo
+    end
+  end
+
+  def set_route
+    if train_repo.count.zero?
+      puts 'Не создано ни одного поезда'
+    elsif route_repo.count.zero?
+      puts 'Не создано ни одного маршрута'
+    else
+      train_repo.each { |t| puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}', #{t.type}" }
+      print 'Укажите какому поезду следует задать маршрут: '
+      train_add_route = train_repo[get_choice(gets.chomp) - 1]
+      route_repo.each do |r|
+        puts "Маршрут #{route_repo.index(r) + 1}."
+        puts r.stations
+      end
+      print 'Укажите маршрут для добавления: '
+      train_add_route.set_route(route_repo[get_choice(gets.chomp) - 1])
+      puts train_add_route.inspect
+    end
+  end
+
+  def add_wagon
+    train_repo.each { |t| puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}' (#{t.type}), вагонов: #{t.wagons.count}" }
+    print 'Укажите к какому поезду следует добавить вагон: '
+    train_add_wagon = train_repo[get_choice(gets.chomp) - 1]
+    wagon_repo.each do |w|
+      next unless w.type == train_add_wagon.type
+
+      wagon_used = wagon_used?(w)
+      unless wagon_used
+        puts "#{wagon_repo.index(w) + 1}. Вагон, тип '#{w.type}'"
+      end
+    end
+    print 'Укажите вагон для добавления к поезду: '
+    wagon_to_add = wagon_repo[get_choice(gets.chomp) - 1]
+    train_add_wagon.add_wagon(wagon_to_add)
+    puts "Количество вагонов поезда #{train_add_wagon.number}: #{train_add_wagon.wagons.count}"
+  end
+
+  def remove_wagon
+    train_repo.each do |t|
+      if t.wagons.count > 0
+        puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}' (#{t.type}), вагонов: #{t.wagons.count}"
+      end
+    end
+    print 'Укажите от какого поезда следует отцепить вагон: '
+    train_remove_wagon = train_repo[get_choice(gets.chomp) - 1]
+    puts "Поезд номер #{train_remove_wagon.number} выбран"
+    train_remove_wagon.remove_wagon(train_remove_wagon.wagons.last)
+    puts "Количество вагонов поезда #{train_remove_wagon.number}: #{train_remove_wagon.wagons.count}"
+  end
+
+  def move_train
+    train_repo.each do |t|
+      unless t.route.nil?
+        puts "#{train_repo.index(t) + 1}. Поезд '#{t.number}', #{t.type}"
+      end
+    end
+    print 'Укажите поезд для передвижения: '
+    train_to_move = train_repo[get_choice(gets.chomp) - 1]
+    puts '1. Движение вперед'
+    puts '2. Движение назад'
+    print 'Укажите направление движения поезда: '
+    direction = get_choice(gets.chomp)
+
+    if direction == 1
+      if train_to_move.station != train_to_move.route.stations.last
+        train_to_move.move_forward
+      else
+        puts 'Поезд на конечной станции'
+      end
+    elsif direction == 2
+      if train_to_move.station != train_to_move.route.stations.first
+        train_to_move.move_backward
+      else
+        puts 'Поезд на начальной станции'
+      end
+    end
+
+    puts "Поезд #{train_to_move.number} находится на станции #{train_to_move.station.name}"
+  end
+
+  def station_monitor
+    station_repo.each do |s|
+      puts "Количество поездов на станции '#{s.name}': #{s.trains.count}"
+      s.trains.each { |t| puts t.number } if s.trains.count > 0
+    end
   end
 
   def get_choice(value)
